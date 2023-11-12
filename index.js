55c730a99c8ca013ed05879c91c8e17e87a68541
Matheus Eyhad Dias sales Costa <dev.eyhad@gmail.com>
2023-11-12 07:27:19 -0300

    meu commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let xpTotal = 45
+let fraseNivel = "Seu nível é "
+
+if(xpTotal < 10){
+    console.log(fraseNivel + "Ferro")
+} else if (xpTotal >= 10 && xpTotal <= 20){
+    console.log(fraseNivel + "Bronze")
+}else if (xpTotal >= 21 && xpTotal <= 30){
+    console.log(fraseNivel + "Prata")
+}else if (xpTotal >= 31 && xpTotal <= 40){
+    console.log(fraseNivel + "Ouro")
+}else if (xpTotal >= 41 && xpTotal <= 50){
+    console.log(fraseNivel + "Diamante")
+}else if (xpTotal >= 51 && xpTotal <= 60){
+    console.log(fraseNivel + "Platina")
+}else if (xpTotal >= 61 && xpTotal <= 70){
+    console.log(fraseNivel + "Mestre")
+}else if (xpTotal >= 71 && xpTotal <= 80){
+    console.log(fraseNivel + "Desafiante")
+} 
+
+
